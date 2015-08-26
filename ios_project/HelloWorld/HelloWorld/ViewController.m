cd533496ea64e66849ea68fc7bb3ed06b3507215
Stephen Spann <stephenspann@gmail.com>
2015-08-25 23:00:36 -0400

    Update ViewController.m

--- ios_project/HelloWorld/HelloWorld/ViewController.m
+++ ios_project/HelloWorld/HelloWorld/ViewController.m
@@ -1,33 +1,46 @@
-//
-//  ViewController.m
-//  HelloWorld
-//
-//  Created by Stephen Spann on 8/13/15.
-//  Copyright (c) 2015 Stephen Spann. All rights reserved.
-//
-
 #import "ViewController.h"
 #import "HWHelloWorld.h"
-
+ 
 @interface ViewController ()
-
+ 
 @end
-
+ 
 @implementation ViewController {
-    HWHelloWorld* _helloWorldInterface;
+    HWHelloWorld *_helloWorldInterface;
+    UIButton *_button;
+    UITextView *_textView;
 }
-
+ 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+     
+    // instantiate our library interface
     _helloWorldInterface = [HWHelloWorld create];
-    NSLog(@"%@", [_helloWorldInterface getHelloWorld]);
-    
+     
+    // create a button programatically for the demo
+    _button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [_button addTarget:self action:@selector(buttonWasPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [_button setTitle:@"Get Hello World!" forState:UIControlStateNormal];
+    _button.frame = CGRectMake(20.0, 20.0, 280.0, 40.0);
+    [self.view addSubview:_button];
+     
+    // create a text view programatically
+    _textView = [[UITextView alloc] init];
+    // x, y, width, height
+    _textView.frame = CGRectMake(20.0, 80.0, 280.0, 380.0);
+    [self.view addSubview:_textView];
+     
 }
-
+ 
+- (void)buttonWasPressed:(UIButton*)sender
+{
+    NSString *response = [_helloWorldInterface getHelloWorld];
+    _textView.text = [NSString stringWithFormat:@"%@\n%@", response, _textView.text];
+}
+ 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+ 
 @end
